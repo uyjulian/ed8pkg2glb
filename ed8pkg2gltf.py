f1e7b815056cb6640f92b239d6f85f8d498afbed
eArmada8 <103985972+eArmada8@users.noreply.github.com>
2023-03-04 17:59:58 -0800

    Support frozen executable status

--- ed8pkg2gltf.py
+++ ed8pkg2gltf.py
@@ -3313,7 +3313,10 @@ def process_pkg(pkg_name, partialmaps = partial_vgmaps_default, allbuffers = Fal
 
 if __name__ == '__main__':
     # Set current directory
-    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    if getattr(sys, 'frozen', False):
+        os.chdir(os.path.dirname(sys.executable))
+    else:
+        os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
     # If argument given, attempt to export from file in argument
     if len(sys.argv) > 1:
